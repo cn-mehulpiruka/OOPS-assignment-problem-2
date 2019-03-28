b87a5843a57624a1e9e6357f6a5528c8d8367793
Condé Nast <condenast@Condes-MacBook-Air.local>
2019-03-28 18:04:53 +0530

    created employer module with classes and methods

--- employer.py
+++ employer.py
@@ -114,6 +114,22 @@ class permanent(Employer):
 	def __str__(self):
 	    return 'salary: '   +   self.salary +   \
               '\ncontract: '   +   self.contract
+class Hr_management(Employer):
+	def __init__(self,num_of_recruitment,finance_description):
+		super().__init__()
+		self.num_of_recruitment = num_of_recruitment
+		self.finance_descriptiom = finance_description
+	def set_num_of_recruitment(self,num_of_recruitment):
+		self.num_of_recruitment = num_of_recruitment
+	def set_finance_description(self,finance_description):
+		self.finance_description = finance_description
+	def get_num_of_recruitment(self):
+		return num_of_recruitment
+	def get_finance_description(self):
+		return finance_description
+	def __str__(self):
+	    return 'num_of_recruitment: '   +   self.num_of_recruitment +   \
+               '\nfinance_description: '   +   self.
 class department:
 	def __init__(self,depname,depid,project_id):
 		self.depname = depname
@@ -135,21 +151,6 @@ class department:
 	    return 'depname: '   +   self.depname  +   \
                '\ndepid: '   +   self.depid   +   \
                '\nproject_id: '    +   self.project_id 
-class Hr_department(Employer):
-	def __init__(self,num_of_recruitment,finance_description):
-		super().__init__()
-		self.num_of_recruitment = num_of_recruitment
-		self.finance_descriptiom = finance_description
-	def set_num_of_recruitment(self,num_of_recruitment):
-		self.num_of_recruitment = num_of_recruitment
-	def set_finance_description(self,finance_description):
-		self.finance_description = finance_description
-	def get_num_of_recruitment(self):
-		return num_of_recruitment
-	def get_finance_description(self):
-		return finance_description
-	def __str__(self):
-	    return 'num_of_recruitment: '   +   self.num_of_recruitment +   \
-               '\nfinance_description: '   +   self.finance_description
+
           
 		

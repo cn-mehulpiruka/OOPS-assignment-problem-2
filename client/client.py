1cf79181871d4301f1fa918a62b4c524e0cc0117
cn-mehulpiruka <condenast@Condes-MacBook-Air.local>
2019-03-28 18:58:42 +0530

    DONEEEEEEEE......!!!

--- client/client.py
+++ client/client.py
@@ -12,10 +12,13 @@ class client:
 		self.total_amountbilled=0
 	@classmethod
 	def billing(cls,inventory,order):
-		time_period=d.date(order.start_date)-d.date(order.end_date)
+		sdate_=d.date(int(order.start_date[0:4]),int(order.start_date[5:7]),int(order.start_date[8:]))
+		edate_=d.date(int(order.end_date[0:4]),int(order.end_date[5:7]),int(order.end_date[8:]))
+		print(edate_- sdate_)
+		time_period=int((str(edate_- sdate_).split(" "))[0])
 		if order.billing_period=='Day':
 			return order.amountpaid*time_period
-		elif order.billing_period=='Month':
+		elif order.billing_period=='Monthly':
 			return (order.amountpaid//30)*time_period
 		else:
 			return (order.amountpaid//365)*time_period
@@ -27,17 +30,21 @@ class client:
 			return 
 		for order in advertiser.orders:
 			flag=False
-			if d.date(order.start_date)<today and today< d.date(order.end_date) and order.status==True:
+			sdate_=d.date(int(order.start_date[0:4]),int(order.start_date[5:7]),int(order.start_date[8:]))
+			edate_=d.date(int(order.end_date[0:4]),int(order.end_date[5:7]),int(order.end_date[8:]))
+			if sdate_<=today and today<=edate_ and order.status==True:
 				for publisher in publishers:
 					if len(publisher.inventories)==0:
 						break
 					for inventory in publisher.inventories:
-						 if inventory.price<order.amountpaid and inventory.status==True:
+						print(inventory.price," ",order.amountpaid)
+						if float(inventory.price)<float(order.amountpaid) and inventory.status==True:
 						 	print("Published")
 						 	amount=client.billing(inventory,order)
 						 	publisher.earned(amount)
 						 	advertiser.spent(amount)
 						 	inventory.status=False
+						 	order.status=False
 						 	flag=True
 						 	break
 					if flag:
@@ -63,30 +70,30 @@ class publisher(client):
 		if tax_entity_=='Coporate':
 			print(tax_entity_)
 			# print("")
-
 		else:
 			tax_entity='Indiviual'
 		self.tax_id_no=tax_id_no_
 		self.client_type="Publisher"
 		self.url=url_
 		self.flight_period=flight_period_
-	def create_inventory(self,size_):
-		new_invent=inventory.create_inventory(size_)
+	def create_inventory(self,size_,price_,status_):
+		new_invent=inventory.create_new_inventory(size_,price_,status_)
 		self.inventories.append(new_invent)
 	def earned(self,amount_):
+		print("earned",self)
 		self.total_amountbilled+=amount_
 
 
 class inventory:
 	_inventory_id=0
 	def __init__(self):
-		inventory.__inventory_id+=1
-		self.__inventory_id=inventory.__inventory_id
+		inventory._inventory_id+=1
+		self._inventory_id=inventory._inventory_id
 		self.size_=0
 		self.price=1.04
 		self.status=True
 	@classmethod
-	def create_inventory(cls,size_,price_,status_=True):
+	def create_new_inventory(cls,size_,price_,status_=True):
 		new_invent=inventory()
 		new_invent.size=size_
 		new_invent.price=price_
@@ -121,11 +128,12 @@ class advertiser(client):
 		self.client_type="Advertiser"
 		self.flight_period=flight_period_
 
-	def create_ad(self,start_date_,end_date_,amountpaid_,billing_period_,status_=False,):
+	def create_ad(self,start_date_,end_date_,amountpaid_,billing_period_,status_=False):
 		new_order=order()
 		new_order.create_order(self.__advertiser_id,start_date_,end_date_,status_,amountpaid_,billing_period_)
 		self.orders.append(new_order)
 	def spent(self,amount_):
+		print("spend",self)
 		self.total_amountbilled+=amount_
 
 class order:
@@ -133,13 +141,13 @@ class order:
 	def __init__(self):
 		order.__order_id+=1
 		self.__order_id=order.__order_id
-		self.start_date=str(now.year)+"-"+str(now.month())+"-"+str(now.day())
-		self.end_date=str(now.year)+"-"+str(now.month())+"-"+str(now.day())
+		self.start_date=str(today.year)+"-"+str(today.month)+"-"+str(today.day)
+		self.end_date=str(today.year)+"-"+str(today.month)+"-"+str(today.day+1)
 		self.status=False
 		self.amountpaid=0
 		self.billing_period="Day"
 		self.advertiser_id=None
-	def create_order(self,advertiser_id_,start_date_,end_date_,status_=False,amountpaid_=1.05,billing_period_="Day",):
+	def create_order(self,advertiser_id_,start_date_,end_date_,status_=True,amountpaid_=1.05,billing_period_="Day",):
 		self.start_date=start_date_
 		self.end_date=end_date_
 		self.status=status_
--- client/using_client.py
+++ client/using_client.py
@@ -1,4 +1,7 @@
 import client as c
+import datetime as d
+
+today = d.date.today()
 
 choice=1
 
@@ -6,15 +9,15 @@ Advertiser=[]
 Publisher=[]
 
 def create_client():
-		print("1) Add as a Publisher \n2) Add as a Advertiser")
+		print("1) Add as a Publisher \n2) Add as a Advertiser ")
 		choice_add=int(input())
-		name_=input("Enter Name of the CLient")
-		flight_period_=int(input("Enter the flight period of the Client"))
+		name_=input("Enter Name of the CLient ")
+		flight_period_=int(input("Enter the flight period of the Client "))
 		print("Choose a Tax Exntity\n 1) Coporate \n 2) Indiviual")
 		tax_entity_= "Coporate" if input()==1 else "Indiviual"
-		tax_id_no_=input("Enter a Tax No") 
+		tax_id_no_=input("Enter a Tax No ") 
 		if choice_add==1:
-			url_=input("Enter url of the Client")
+			url_=input("Enter url of the Client ")
 			p=c.publisher()
 			p.create_publisher(url_,flight_period_,name_,tax_entity_,tax_id_no_)
 			print(dir(c.publisher))
@@ -56,15 +59,88 @@ def publish():
 		publish_advertiser_id=int(input("Enter The advertiser id to publish: "))
 		# print(Advertiser[publish_advertiser_id-1])
 		c.client.publish(Publisher,Advertiser[publish_advertiser_id-1])
-		
 def create_addons():
-	pass
+	ch=int(input("What do want to create ?\n 1) Inventory \n 2) Order"))
+	if ch==1:
+		create_inventory()
+	elif ch==2:
+		create_order()
+	else:
+		print("Wrong options..!! Starting again..!!")
+	
 def create_order():
-	pass
+	print("Advertiser ID\t\t Advertiser Name")
+	cnt=1
+	for advertiser in Advertiser:
+			print(cnt,"\t\t",advertiser.name)
+			cnt+=1
+	advertiser_id=int(input("Enter The advertiser id for which you want to create order: "))
+	order_4_advertiser= (Advertiser[advertiser_id-1])
+	start_date_=input("Enter start date for the order : (press 1 for today) (YYYY-MM-DD)")
+	if start_date_=="1":
+		start_date_=str(today)
+	print(start_date_)
+
+	end_date_=input("Enter end date for the order: (YYYY-MM-DD):  ")
+	amountpaid_=float(input("Enter amount advertiser would be paying : (enter amount in 00.00): "))
+	ch=(input("Choose your desire billing period\n 1) Day \n 2) Monthly \n 3) Annually: \n"))
+	billing_period_= "Day" if  ch== "1" else  ("Monthly" if ch=="2" else "Annually" )
+	print(billing_period_)
+	if start_date_!= today and d.date(int(start_date_[0:4]),int(start_date_[5:7]),int(start_date_[8:]))< today:
+		status_=True
+	else:
+		status_=False
+
+	order_4_advertiser.create_ad(start_date_,end_date_,amountpaid_,billing_period_,status_)
+	print(order_4_advertiser.__dict__)
+
 def create_inventory():
-	pass
-def show_amoutn(client):
-	pass
+	print("Publishers ID\t\t Publishers Name")
+	cnt=1
+	for publisher in Publisher:
+			print(cnt,"\t\t",publisher.name)
+			cnt+=1
+	publisher_id=int(input("Enter The publisher id for which you want to create Inventory: "))
+	inventory_of_publisher= (Publisher[publisher_id-1])
+	size_=input("Enter size in x * Y format: ")
+	price_=input("Enter price you are excepting: ")
+	inventory_of_publisher.create_inventory(size_,price_,True)
+	print(inventory_of_publisher.__dict__)
+
+def show_amoutn():
+	ch=int(input("\nChoose the client whose you want to see the amount\n1) Publishers\n2) Advertisers \n3) All\n"))
+	cnt=1
+	if ch==1 or ch==3:
+		print("Publishers ID\t\t Publishers Name")
+		for publisher in Publisher:
+				print(cnt,"\t\t",publisher.name)
+				cnt+=1
+	if ch==2 or ch==3:
+		print("Advertiser ID\t\t Advertiser Name")
+		for advertiser in Advertiser:
+				print(cnt,"\t\t",advertiser.name)
+				cnt+=1
+	
+	cnt=1
+	if ch==3:
+		print("Publishers ID\t\t Publishers Name")
+		for publisher in Publisher:
+				print(cnt,"\t\t",publisher.name,"\t\t",publisher.total_amountbilled)
+				cnt+=1
+		print("Advertiser ID\t\t Advertiser Name\t\t")
+		for advertiser in Advertiser:
+				print(cnt,"\t\t",advertiser.name,"\t\t",advertiser.total_amountbilled)
+				cnt+=1
+	else:
+		client_id=int(input("Enter the no corespondingto the client "))
+		if ch==1:
+			obj=Publisher[client_id-1]
+		else:
+			obj=Advertiser[client_id-1]
+		print("Total Amount: ",obj.total_amountbilled)
+
+
+
 def go_back():
 	pass
 
@@ -75,12 +151,11 @@ while choice >0 and choice<10:
 	print('What to you want to do?')
 	print(" 1) Add Client \n 2) View Client \n 3) Show Amount \n 4) Publish \n 5) Add addons \n 6) Go Back")
 	choice=int(input())
-	if choice>5 or choice<1:
+	if choice>6 or choice<1:
 		print("Choose a valid option:")
 	elif choice==1:
 		create_client()
-	elif choice==4:
-		publish()
+	
 	elif choice==2:
 		print("1) View All CLinets\n2) View Publisher\n3) View Advertisers")
 		ch=int(input())
@@ -96,6 +171,13 @@ while choice >0 and choice<10:
 				print ("===No Publishers.!! Get some===\n")
 			else:
 				view_clients(Publisher)
+	elif choice==3:
+		show_amoutn()
+	elif choice==4:
+		publish()
+	elif choice==5:
+		create_addons()
+
 	elif choice==6:
 		exit()
 

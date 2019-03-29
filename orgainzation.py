8e88f078f5275bdda08d4a7472d4f943d725cf87
cn-mehulpiruka <mehulkumar_piruka@condenast.com>
2019-03-29 14:57:47 +0530

    Update orgainzation.py

--- orgainzation.py
+++ orgainzation.py
@@ -1,12 +1,24 @@
+import os
 import client.client as c
+  def welcome():
+ 	ch=int(input("1) Company \n2) Employees\n3) Client \n4) Exit\n  :"))
+ 	if ch==1:
+ 		pass
+ 	elif ch==2:
+ 		pass
+ 	elif ch==3:
+ 		print("Reaady to go with the client dashboard??? ")
+ 		print(dir(c))
+ 		c.client_working()
+ 	else:
+ 		exit()
 
-ch=int(input("1) Company \n2) Employees\n3) Client \n4) Exit\n  :"))
-if ch==1:
-	pass
-elif ch==2:
-	pass
-elif ch==3:
-	print("Reaady to go with the client dashboard??? ")
-	c.client_working()
-else:
-	exit()
+  if __name__=='__main__':
+ 	if os.name=='posix':
+ 		os.system('clear')
+ 	print("=**="*50)
+ 	print("=**="*50)
+ 	print("=**="*20,"Welcome to the trainees made project ","=**="*20)
+ 	print("=**="*50)
+ 	print("=**="*50)
+ 	welcome()
